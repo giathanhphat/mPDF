04401001005c5f383606d52019ecba779a4307ea
giathanhphat <gtphat94@gmail.com>
2017-06-25 23:32:04 +0700

    test thêm hàm của mPDF

--- test.php
+++ test.php
@@ -1,129 +1,41 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Print Invoice</title>
+    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="test.css">
+   <!--  <title>Document</title> -->
 </head>
 <body>
-<div id="wrapper">
-     
-    <p style="text-align:center; font-weight:bold; padding-top:5mm;">INVOICE</p>
-    <br />
-    <table class="heading" style="width:100%;">
-        <tr>
-            <td style="width:80mm;">
-                <h1 class="heading">ABC Corp</h1>
-                <h2 class="heading">
-                    123 Happy Street<br />
-                    CoolCity - Pincode<br />
-                    Region , Country<br />
-                     
-                    Website : www.website.com<br />
-                    E-mail : info@website.com<br />
-                    Phone : +1 - 123456789
-                </h2>
-            </td>
-            <td rowspan="2" valign="top" align="right" style="padding:3mm;">
-                <table>
-                    <tr><td>Invoice No : </td><td>11-12-17</td></tr>
-                    <tr><td>Dated : </td><td>01-Aug-2011</td></tr>
-                    <tr><td>Currency : </td><td>USD</td></tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <b>Buyer</b> :<br />
-                Client Name<br />
-            Client Address
-                <br />
-                City - Pincode , Country<br />
-            </td>
-        </tr>
-    </table>
-         
-         
-    <div id="content">
-         
-        <div id="invoice_body">
-            <table>
-            <tr style="background:#eee;">
-                <td style="width:8%;"><b>Sl. No.</b></td>
-                <td><b>Product</b></td>
-                <td style="width:15%;"><b>Quantity</b></td>
-                <td style="width:15%;"><b>Rate</b></td>
-                <td style="width:15%;"><b>Total</b></td>
-            </tr>
-            </table>
-             
-            <table>
-            <tr>
-                <td style="width:8%;">1</td>
-                <td style="text-align:left; padding-left:10px;">Software Development<br />Description : Upgradation of telecrm</td>
-                <td class="mono" style="width:15%;">1</td><td style="width:15%;" class="mono">157.00</td>
-                <td style="width:15%;" class="mono">157.00</td>
-            </tr>         
-            <tr>
-                <td colspan="3"></td>
-                <td></td>
-                <td></td>
-            </tr>
-             
-            <tr>
-                <td colspan="3"></td>
-                <td>Total :</td>
-                <td class="mono">157.00</td>
-            </tr>
-        </table>
+    <div class="wrapper">
+        <div class="trai">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat dolorem aliquam enim quas fugit temporibus, voluptatibus sunt sequi porro quasi tenetur mollitia ipsa cum laboriosam voluptatum iusto magnam, ipsum eum, obcaecati illo facere! Tenetur accusantium laborum voluptatibus facere pariatur atque nulla quas sed vel repellendus eius ducimus qui adipisci accusamus architecto id omnis vitae totam commodi, nostrum nam quos, soluta itaque quod maxime? Nihil iure officiis tenetur obcaecati ea incidunt modi, est ipsa esse fugiat facilis neque veritatis dolores libero numquam consectetur perferendis, exercitationem beatae, porro eius laboriosam! Nisi cumque saepe necessitatibus et vel repellat neque quia pariatur hic possimus, delectus esse. Deleniti repellat architecto odio, doloremque explicabo et amet suscipit corrupti assumenda, dicta placeat hic libero enim magni sed reprehenderit facilis eum sit? Recusandae culpa asperiores esse illo, molestias, id inventore, laboriosam vitae doloribus distinctio corporis pariatur maxime iusto quam saepe dolores! Ipsum saepe veniam eius laudantium atque maiores laboriosam, provident architecto quos iusto consequuntur optio quaerat? Ex, rerum ullam placeat incidunt, officia facere aut deleniti pariatur sequi sint necessitatibus asperiores nesciunt reiciendis minima quas consectetur! Minus dignissimos quo veniam autem laborum beatae. Saepe numquam, optio dignissimos accusamus sequi, doloribus libero delectus, nulla quia molestiae quos illum vero laudantium repellat, officiis incidunt cum vitae. Nesciunt fuga repellat dolorum incidunt tempora libero id, repudiandae culpa sit eos magni ducimus animi commodi ipsam tempore in ipsum voluptatem consectetur mollitia quibusdam nisi asperiores reprehenderit, itaque. Quas laudantium inventore, natus, placeat incidunt cum eligendi, harum voluptates a maiores similique quaerat recusandae minima rerum, eos consectetur. Molestias, fugiat mollitia voluptatibus quod. Rerum at pariatur qui labore, eaque laudantium porro hic quas consequatur soluta architecto, necessitatibus, eius nisi reiciendis ducimus fuga quidem consectetur iure ratione atque enim quod quam? Inventore pariatur, nemo corporis eligendi dignissimos quisquam quaerat dolores, nam cupiditate cumque nulla? Aliquam tempore, excepturi!
+          necessitatibus, eius nisi reiciendis ducimus fuga quidem consectetur iure ratione atque enim quod quam? Inventore pariatur, nemo corporis eligendi dignissimos quisquam quaerat dolores, nam cupiditate cumque nulla? Aliquam tempore, excepturi!
+          enim quod quam? Inventore pariatur, nemo corporis eligendi dignissimos quisquam quaerat dolores, nam cupiditate cumque nulla? Aliquam tempore, excepturi!
         </div>
-        <div id="invoice_total">
-            Total Amount :
-            <table>
+        <?php $x = 'haha'; ?>
+        <div class="phai">
+            <table border="1" cellspacing="0">
                 <tr>
-                    <td style="text-align:left; padding-left:10px;">One  Hundred And Fifty Seven  only</td>
-                    <td style="width:15%;">USD</td>
-                    <td style="width:15%;" class="mono">157.00</td>
+                    <td>co len</td>
+                    <td>co len</td>
+                    <td>co len</td>
+                    <td>co len</td>
+                     <td>co len</td>
+                      <td>co len</td>
+                       <td>co len</td>
+                        <td>co len</td>
+                         <td>co len</td>
+                          <td>co len</td>
+                           <td>co len</td>
+                            <td>co len</td>
+                             <td>co len</td>
                 </tr>
+                
+
             </table>
         </div>
-        <br />
-        <hr />
-        <br />
-         
-        <table style="width:100%; height:35mm;">
-            <tr>
-                <td style="width:65%;" valign="top">
-                    Payment Information :<br />
-                    Please make cheque payments payable to : <br />
-                    <b>ABC Corp</b>
-                    <br /><br />
-                    The Invoice is payable within 7 days of issue.<br /><br />
-                </td>
-                <td>
-                <div id="box">
-                    E &amp; O.E.<br />
-                    For ABC Corp<br /><br /><br /><br />
-                    Authorised Signatory
-                </div>
-                </td>
-            </tr>
-        </table>
+        <div class="clear"></div>
+        <div class="giua"><?php echo $x; ?></div>
     </div>
-     
-    <br />
-     
-    </div>
-     
-    <!-- <htmlpagefooter name="footer">
-        <hr />
-        <div id="footer"> 
-            <table>
-                <tr><td>Software Solutions</td><td>Mobile Solutions</td><td>Web Solutions</td></tr>
-            </table>
-        </div>
-    </htmlpagefooter>
-    <sethtmlpagefooter name="footer" value="on" /> -->
-     
 </body>
 </html>
- 
