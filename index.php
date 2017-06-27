43ac6a7bad67afc2fb0f33f8d12fa2276aa0af59
giathanhphat <gtphat94@gmail.com>
2017-06-28 00:37:59 +0700

    update file.

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 //16 trang mất 54s -->
-//trang A4 có chiều rộng 675px
+//36 trang mất 1p53s
+//trang A4 có chiều rộng 675px, chiều cao 1000px
 //thẻ div sẽ bao bọc toàn bộ kí tự cho dù có height = 0 (khác với khi xuất html);
 //khi vượt quá một trang nội dung hiển thị vẫn tự dộng canh lề như đã chỉnh
 //tự động thêm trang mới nếu vượt quá 1 trang
@@ -118,5 +119,40 @@ require_once __DIR__ . '/vendor/autoload.php';
 // This text will only be visible on screen, and will not be included when the document is printed
 // ');
 // $mpdf->Output();
+	$mpdf = new mPDF();
+	//set default cho header and footer
+	$mpdf->defaultheaderfontsize = 10;
+	$mpdf->defaultheaderfontstyle = 'B';
+	$mpdf->defaultheaderline = 0;
+	$mpdf->defaultfooterfontsize = 10;
+	$mpdf->defaultfooterfontstyle = 'BI';
+	$mpdf->defaultfooterline = 0;
+	$mpdf->defaultPageNumStyle = 'A';//giống hiển thị như A, B, C hoặc 1, 2, 3 hoặc I, II, III...
+	$mpdf->defaultAlign = 'L';
+	$mpdf->defaultCSS;
+	$mpdf->default_font;
+	$mpdf->default_lang;
+	$mpdf->defaultPagebreakType = 'BLANK';
+	$mpdf->defaultSubsFont;
+	$mpdf->defaultTableAlign;
+	$mpdf->default_available_fonts;
+	$mpdf->default_font_size;
+
+	//trang đầu tạo mặc định
+	//header and footer được set trước khi thêm vào trang
+	$mpdf->SetHeader('Hoc report PDF');
+	$mpdf->SetFooter('Page {PAGENO}/{nb}');
+	// page first được thêm ở đây
+	$html = file_get_contents('test.php');
+	$mpdf->WriteHTML($html, 1);
+	$mpdf->WriteHTML($html, 2);
+	// đây là header của page 2, vì header set trước nên đặt trên AddPage()
+	$mpdf->SetHeader('Hoc report PDF1');
+	// đây là page trang 2
+	$mpdf->AddPage();
+	$mpdf->WriteHTML($html, 1);
+	$mpdf->WriteHTML($html, 2);
+	$mpdf->AliasNbPages();
+	$mpdf->Output();
 ?>
 
